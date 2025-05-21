ae67c852c36336d20d2d1e3f680c538178e95a32
Chelsjavis <chelsea.davis975@gmail.com>
2025-05-21 21:43:41 +1000

    Add axios, update search functionality
    Update JS to change city and temperature elements when city is searched.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
+function updateWeather(response) {
+  let cityElement = document.querySelector("#city-element");
+  cityElement.innerHTML = response.data.city;
+
+  let temperatureElement = document.querySelector("#temperature-element");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "1894f4b60349tcab94fb26933d94a5o1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function weatherSearchSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#search-form-input");
-
-  let cityElement = document.querySelector("#city-element");
-  cityElement.innerHTML = city.value;
+  let cityInput = document.querySelector("#search-form-input");
+  searchCity(cityInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-
 searchFormElement.addEventListener("submit", weatherSearchSubmit);
+
+searchCity("Sydney");
