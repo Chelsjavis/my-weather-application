6b87955d2fb0ab6c89898cd61e81da3f1f69b1b3
Chelsjavis <chelsea.davis975@gmail.com>
2025-05-28 21:26:51 +1000

    Inject humidity, wind speed and weather description

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 function updateWeather(response) {
   let cityElement = document.querySelector("#city-element");
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 
   let temperatureElement = document.querySelector("#temperature-element");
   let temperature = Math.round(response.data.temperature.current);
